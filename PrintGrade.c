53fbbd21dc868ec0ff6544245c327f28a52e6fe4
rookiehome7 <rookiehome7@users.noreply.github.com>
2018-11-20 17:03:40 +0700

    Homeuploaded2

--- PrintGrade.c
+++ PrintGrade.c
@@ -1,8 +1,10 @@
+//home version eiei 
 #include <stdio.h>
+
 void main()
 {
   char Name[30][40] = {"Sam Matthews", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"}; 
-  int ID[30] = {5301, 5302, 5303, 5304, 5305, 5306}; 
+  int ID[30] = {3401, 3402, 3405, 3410, 3450, 3470}; 
   float GPA[30] = {2.73, 2.35, 3.65, 3.24, 1.32, 3.26}; 
   int N = 6, i;
   int SearchID, found_index; 
