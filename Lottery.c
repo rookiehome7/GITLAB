ce6e9ad1442a15ad78fbb8a844405def1a144690
rookiehome7 <rookiehome7@users.noreply.github.com>
2018-11-20 16:55:29 +0700

    Home

--- Lottery.c
+++ Lottery.c
@@ -4,7 +4,8 @@ void main()
   int WinLotteryNo[10] = {1522, 1711, 5515, 7233, 5614, 8876, 1215, 4232, 9155, 9995}; 
   float WinAmount[10] = {50000, 10000, 10000, 5000, 5000, 5000, 5000, 3000, 3000, 3000};
   int N = 10;
-  int i, won; 
+  int i, won;
+  int dumpVariable = 0; 
   
   int YourLotteryNo;  
    
@@ -22,7 +23,7 @@ void main()
        }
     }
     if (!won) 
-      printf("Sorry Lottery Number %4d did not win\n", YourLotteryNo); 
+      printf("Sorry, Your money gone hahahahahahah : Lottery Number %4d did not win\n", YourLotteryNo); 
     printf("Enter your Lottery Number (0 to end): "); 
     scanf("%d", &YourLotteryNo); 
   }
